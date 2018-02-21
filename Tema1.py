9f9f84953913ba105f352f3dc3f31463f9748602
mihaivasilache <mihai.vasilache12@yahoo.com>
2018-02-21 14:21:23 +0200

    Solved all problems. Reformated code.

--- Tema1.py
+++ Tema1.py
@@ -1,123 +1,118 @@
 import numpy as np
 
 """ Problema 1 """
-def machinePrecisionSum():
+
+
+def machine_precision_sum():
     m = -1
-    while (True):
-        if (1.0 + pow(10, m) == 1.0):
+    while True:
+        if 1.0 + pow(10, m) == 1.0:
             return pow(10, m + 1), (m + 1)
-        m -= 1;
-#print("Final result is stated for u = {0} and power = {1}".format(machinePrecisionSum()[0], machinePrecisionSum()[1]))
+        m -= 1
+
 
 """ Problema 2 """
-def checkNonAssociativity(x = 1.0):
-    y = z = machinePrecisionSum()[0]
+
+
+def check_non_associativity(x=1.0):
+    y = z = machine_precision_sum()[0]
     boolean_value = ((x + y) + z == x + (y + z))
     if boolean_value is False:
-        print("The operation is non-associative")
+        return "The sum operation is non-associative"
     else:
-        print("The operation is associative")
-#print(checkNonAssociativity())
+        return "The sum operation is associative"
 
-def checkMultiplicationAssociativity():
-    y = z = machinePrecisionSum()[0]
+
+def check_multiplication_associativity():
+    y = z = machine_precision_sum()[0]
     x = 1.0
     boolean_value = ((x * y) * z == x * (y * z))
     while boolean_value:
         x = x * 1.1
         boolean_value = ((x * y) * z == x * (y * z))
     return x, (x * y) * z, x * (y * z)
-#print(checkMultiplicationAssociativity())
+
 
 """ Problema 3 """
 """
     Implementation of the strassen algorithm, similar to
     http://en.wikipedia.org/w/index.php?title=Strassen_algorithm&oldid=498910018#Source_code_of_the_Strassen_algorithm_in_C_language
 """
-def strassen(A, B):
-    n = len(A)
-    if n==1:
-        C = np.multiply(A, B)
-    else:
-        new_size = n//2
-        """ Initialize the submatrices """
-        a11 = np.zeros(shape=(new_size, new_size))
-        a12 = np.zeros(shape=(new_size, new_size))
-        a21 = np.zeros(shape=(new_size, new_size))
-        a22 = np.zeros(shape=(new_size, new_size))
-        b11 = np.zeros(shape=(new_size, new_size))
-        b12 = np.zeros(shape=(new_size, new_size))
-        b21 = np.zeros(shape=(new_size, new_size))
-        b22 = np.zeros(shape=(new_size, new_size))
 
-        aResult = np.zeros(shape=(new_size, new_size))
-        bResult = np.zeros(shape=(new_size, new_size))
 
-        """ Devide the matrices in 4 submatrices """
-        for i in range(0, new_size):
-            for j in range(0, new_size):
-                """ Bottom left """
-                a11[i][j] = A[i][j]
-                """ Bottom right """
-                a12[i][j] = A[i][j + new_size]
-                """ Down left """
-                a21[i][j] = A[i + new_size][j]
-                """ Down size """
-                a22[i][j] = A[i + new_size][j + new_size]
+def multiply_strassen(matrix_a, matrix_b, n, n_min):
+    if n <= n_min:
+        return np.dot(matrix_a, matrix_b)
 
-                """ Bottom left """
-                b11[i][j] = B[i][j]
-                """ Bottom right """
-                b12[i][j] = B[i][j + new_size]
-                """ Down left """
-                b21[i][j] = B[i + new_size][j]
-                """ Down size """
-                b22[i][j] = B[i + new_size][j + new_size]
+    new_size = n // 2
+    """ Initialize the submatrices """
+    a11 = np.zeros(shape=(new_size, new_size))
+    a12 = np.zeros(shape=(new_size, new_size))
+    a21 = np.zeros(shape=(new_size, new_size))
+    a22 = np.zeros(shape=(new_size, new_size))
+    b11 = np.zeros(shape=(new_size, new_size))
+    b12 = np.zeros(shape=(new_size, new_size))
+    b21 = np.zeros(shape=(new_size, new_size))
+    b22 = np.zeros(shape=(new_size, new_size))
 
-                aResult = np.add(a11, a22)
-                bResult = np.add(b11, b22)
-                p1 = strassen(aResult, bResult)
-                
-                aResult = np.subtract(a21, a22)
-                p2 = strassen(aResult, b11)
-                
-                bResult = np.subtract(b12, b22)
-                p3 = strassen(a11, bResult)
-                
-                bResult = np.subtract(b21, b11)
-                p4 = strassen(a22, bResult)
-                
-                aResult = np.add(a11, a12)
-                p5 = strassen(aResult, b22)
-                
-                aResult = np.subtract(a21, a11)
-                bResult = np.add(b11, b12)
-                p6 = strassen(aResult, bResult)
-                
-                aResult = np.subtract(a12, a22)
-                bResult = np.subtract(b21, a22)
-                p7 = strassen(aResult, bResult)
-                
-                c11 = p1 + p4 - p5 + p7
-                c12 = p3 + p5
-                c21 = p2 + p4
-                c22 = p1 + p3 - p2 + p6
-                
-                C = np.zeros(shape=(n, n))
-                for i in range(0, new_size):
-                    for j in range(0, new_size):
-                        C[i][j] = c11[i][j]
-                        C[i][j + new_size] = c12[i][j]
-                        C[i + new_size][j] = c21[i][j]
-                        C[i + new_size][j + new_size] = c22[i][j]
-                        
-                return C
+    """ Devide the matrices in 4 submatrices """
+    for i in range(0, new_size):
+        for j in range(0, new_size):
+            """ Bottom left """
+            a11[i][j] = matrix_a[i][j]
+            """ Bottom right """
+            a12[i][j] = matrix_a[i][j + new_size]
+            """ Down left """
+            a21[i][j] = matrix_a[i + new_size][j]
+            """ Down size """
+            a22[i][j] = matrix_a[i + new_size][j + new_size]
 
-if __name__ == "__main__":                
-    A = np.zeros(shape=(8, 8))
-    B = np.zeros(shape=(8, 8))
-    for i in range(0, 8):
-        for j in range(0, 8):
-            A[i][j] = i
-            B[i][j] = j          
-    print(strassen(A, B))
+            """ Bottom left """
+            b11[i][j] = matrix_b[i][j]
+            """ Bottom right """
+            b12[i][j] = matrix_b[i][j + new_size]
+            """ Down left """
+            b21[i][j] = matrix_b[i + new_size][j]
+            """ Down size """
+            b22[i][j] = matrix_b[i + new_size][j + new_size]
+
+    p1 = multiply_strassen((a11 + a22), (b11 + b22), new_size, n_min)
+    p2 = multiply_strassen((a21 + a22), b11, new_size, n_min)
+    p3 = multiply_strassen(a11, (b12 - b22), new_size, n_min)
+
+    p4 = multiply_strassen(a22, (b21 - b11), new_size, n_min)
+    p5 = multiply_strassen((a11 + a12), b22, new_size, n_min)
+    p6 = multiply_strassen((a21 - a11), (b11 + b12), new_size, n_min)
+    p7 = multiply_strassen((a12 - a22), (b21 + b22), new_size, n_min)
+
+    c11 = p1 + p4 - p5 + p7
+    c12 = p3 + p5
+    c21 = p2 + p4
+    c22 = p1 + p3 - p2 + p6
+
+    matrix_c = np.zeros(shape=(n, n))
+    for i in range(0, new_size):
+        for j in range(0, new_size):
+            matrix_c[i][j] = c11[i][j]
+            matrix_c[i][j + new_size] = c12[i][j]
+            matrix_c[i + new_size][j] = c21[i][j]
+            matrix_c[i + new_size][j + new_size] = c22[i][j]
+
+    return matrix_c
+
+
+if __name__ == "__main__":
+    matrix_size = 8
+    n_min = 1
+    A = np.zeros(shape=(matrix_size, matrix_size))
+    B = np.zeros(shape=(matrix_size, matrix_size))
+    for init_i in range(0, matrix_size):
+        for init_j in range(0, matrix_size):
+            A[init_i][init_j] = init_i
+            B[init_i][init_j] = init_j
+
+    print("Final result is stated for u = {0} and power = {1}".format(machine_precision_sum()[0],
+                                                                      machine_precision_sum()[1]))
+    print(check_non_associativity())
+    print("The product operation is associative for : ", check_multiplication_associativity())
+    print(multiply_strassen(A, B, matrix_size, n_min))
