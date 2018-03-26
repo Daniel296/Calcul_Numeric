f4d5f83fec11a105647c9021de7c0a1423aa12c7
Georgiana Ingrid STOLERU <istoleru@ias.bitdefender.biz>
2018-03-26 16:41:33 +0300

    Tema 4 - Parsing

---
+++ Tema4/Tema4.py
@@ -0,0 +1,51 @@
+
+EPS = pow(10, -7)
+
+
+def readFromFile(file_path):
+    fd = open(file_path, 'rb')
+    """ Get matrix size """
+    matrix_size = int(fd.readline().strip())
+    """ Skip the white line between matrix size and elements of vector b """
+    fd.readline()
+    """ Get elements of b """
+    b = []
+    for i in range(matrix_size):
+        b.append(float(fd.readline().strip()))
+    """ Skip the white line between elements of b and elements of a """
+    fd.readline()
+    """ Get elements of A """
+    """ Initializarea matricii A"""
+    A = [[] for _ in range(matrix_size)]
+    while True:
+        line = fd.readline()
+        if line == '':
+            break
+        """ Get all the elements in one line """
+        el_in_line = line.strip().split(', ')
+        value = float(el_in_line[0])
+        line_index = int(el_in_line[1])
+        column_index = int(el_in_line[2])
+        already_exist = False
+        if len(A[line_index]) > 0:
+            for pair in A[line_index]:
+                """ If I have an element on the same position as another one, I add the two values """
+                if column_index == pair[1]:
+                    already_exist = True
+                    break
+                if column_index == line_index:
+                    if value == 0:
+                        print("Value 0 found on diagonal, for column index equal to: {0} and line index equal to: {1}".format(line_index, column_index))
+                        exit(0)
+        if already_exist == True:
+            pair[0] += value
+        else:
+            A[line_index].append([value, column_index])
+
+    return matrix_size, A, b
+
+def main():
+    matrix_size, A, b = readFromFile("m_rar_2018_5.txt")
+
+if __name__ == '__main__':
+    main()
